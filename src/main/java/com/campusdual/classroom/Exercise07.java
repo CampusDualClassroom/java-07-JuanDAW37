096c76b0c3ec9fec4dd4f3dc214eb9fbd8aa03cf
JuanDAW37 <jymtr1968@gmail.com>
2024-09-27 13:56:24 +0200

    Exercicio 07 listo

--- src/main/java/com/campusdual/classroom/Exercise07.java
+++ src/main/java/com/campusdual/classroom/Exercise07.java
@@ -1,10 +1,16 @@
 package com.campusdual.classroom;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Exercise07 {
 
     public static void main(String[] args) {
-
+        positionInAList(7);
+        positionInAList(11);
+        sumFirstNaturalNumbers(6);
+        showFirstNaturalNumbers(8);
     }
 
     //TODO ↓
@@ -14,19 +20,49 @@ public class Exercise07 {
     // Si lo encuentra, que imprima el siguiente mensaje → El elemento X se encuentra en la posición: Y
     // Si NO lo encuentra, que imprima el siguiente mensaje → El elemento X no se encuentra en la lista.
     public static void positionInAList(int num) {
-
+        String mensaje = "";
+        List<Integer> n = new ArrayList<>();
+        for(int i = 10 ; i > 0; i--){
+            n.add(i);
+        }
+        for(int numero :n){
+            if(numero == num){
+                mensaje = "El elemento " + num + " se encuentra en la posición: " + n.indexOf(num-1);
+                break;
+            }else{
+                mensaje = "";
+            }
+        }
+        if(mensaje.equals("")){
+            System.out.println("El elemento " + num + " no se encuentra en la lista.");
+        }else{
+            System.out.println(mensaje);
+        }
     }
 
     //TODO ↓
     // Que imprima como mensaje la suma de los primeros N números positivos
     public static void sumFirstNaturalNumbers(int num) {
-
+        int suma = 0;
+        if(num > 0){
+            for(int i = 0; i <= num; i++){
+                if(i > 0){
+                    suma +=i;
+                }
+            }
+        }
+        System.out.println(suma);
     }
 
     //TODO ↓
     // Que imprima por pantalla los N primeros números positivos
     public static void showFirstNaturalNumbers(int num) {
-
+        if(num > 0){
+            for(int i = 0; i <= num; i++){
+                if(i > 0){
+                    System.out.print(i + ",");
+                }
+            }
+        }
     }
-
 }
